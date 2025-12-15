6c1e396ef883cfb7415a88d08f4a31ea20622d90
Killian Van Acker <killian.vanacker@student.ucll.be>
2025-12-15 15:35:48 +0100

    joepie de poepie

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import pygame
-def main():
-    
 
+def main():
     pygame.init()
 
     screen_size = (1024, 768)
 
     surface, screen = create_main_surface(screen_size)
 
-    pygame.draw.circle(surface, (255, 100, 100), (512, 384), 80)
-    screen.blit(surface, (0, 0))
-    pygame.display.flip()
+    render_frame(surface, screen)
 
 
 def create_main_surface(screen_size):
     display = pygame.display.set_mode(screen_size)
     surface = pygame.Surface(screen_size)
     return surface, display
+
+
+def render_frame(surface, screen):
+    pygame.draw.circle(surface, (255, 100, 100), (512, 384), 80)
+    screen.blit(surface, (0, 0))
+    pygame.display.flip()
 
 
 main()
