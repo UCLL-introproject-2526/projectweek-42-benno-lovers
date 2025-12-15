a1899b368adbe5cb7e554f47e23db92e1047ec45
Artan helewaut <artan.helewaut@student.ucll.be>
2025-12-15 14:35:37 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+def main():
+    import pygame
+
+
+    # Initialize Pygame
+    pygame.init()
+
+    # Tuple representing width and height in pixels
+    screen_size = (1024, 768)
+
+    # Create window with given size
+    pygame.display.set_mode(screen_size)
+
+
+    def create_main_surface():
+        pygame.display.set_mode(screen_size)
+        return pygame.Surface(screen_size)
+
+    while True:
+        pass
